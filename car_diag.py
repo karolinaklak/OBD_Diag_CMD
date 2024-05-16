a0be0173e270bbd9f29e6cbdf5fae159e9079649
karolinaklak <karolinaklak@live.fr>
2024-05-16 11:40:18 +0200

    finalized a first version of the car_diag.py file // we can access trouble codes, cleared by default, acces engine details on real time. V2 would be to have human-computer conversation, interaction

--- car_diag.py
+++ car_diag.py
@@ -18,6 +18,34 @@ calibration_verification_number = connection.query(obd.commands.CALIBRATION_VERI
 print("Vehicle information : ")
 print("Vin number is : " + vin.value)
 print("Calibration ID : ", calibration_id.value)
+print("Calibration verification number : ", calibration_verification_number.value)
+
+
+#get the engine trouble codes
+codes = connection.query(obd.commands.GET_DTC)
+
+print ("\n Engine trouble codes : ")
+for code in codes.value:
+    print(code)
+    
+# Delete the trouble codes
+connection.query(obd.commands.CLEAR_DTC)
+print("Trouble codes cleared")
+
+
+# Get the engine details in real time
+while True :
+    rpm = connection.query(obd.commands.RPM) # RPM = Revolutions Per Minute
+    speed = connection.query(obd.commands.SPEED) # Speed = Speed in km/h
+    load = connection.query(obd.commands.ENGINE_LOAD) # Load = Engine load in %
+    coolant_temperature = connection.query(obd.commands.COOLANT_TEMP) # Coolant temperature in Celsius
+    
+    print("RPM : ", rpm.value)
+    print("Speed : ", speed.value)
+    print("Engine load : ", load.value)
+    print("Coolant temperature : ", coolant_temperature.value)
+    
+    time.sleep(1)
 
 
 
